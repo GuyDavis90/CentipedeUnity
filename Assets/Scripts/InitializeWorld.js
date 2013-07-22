546979b8a6e18a0740225d95ce84475d46abc4bb
Guy Davis <guy@momentusmobile.com>
2013-07-23 01:09:48 +1200

    Rock walls
    Added rock walls, removed corner rocks from world
    collisions from centipedes are logged, rocks are named after the wall
    they are part of

--- Assets/Scripts/InitializeWorld.js
+++ Assets/Scripts/InitializeWorld.js
@@ -10,6 +10,25 @@ function Start () {
 			playerInstance.parent = playerStart.transform;
 		}
 	}
+	var rocks:GameObject[] = new GameObject[4];
+	for (var o:int = 0; o < 4; o++) {
+		rocks[o] = Resources.Load("Rocks/Rock" + o, GameObject);
+	}
+	var rock:GameObject;
+	for (var r:int = -16; r < 16; r++) {
+		// Rocks north wall
+		rock = Instantiate(rocks[Random.Range(0, 4)], Vector3(r * 1.0f, 0.0f, 16.0f), Quaternion.Euler(0.0f, Random.value * 360.0f, 0.0f));
+		rock.name = "NorthWall";
+		// Rocks east wall
+		rock = Instantiate(rocks[Random.Range(0, 4)], Vector3(16.0f, 0.0f, r * 1.0f), Quaternion.Euler(0.0f, Random.value * 360.0f, 0.0f));
+		rock.name = "EastWall";
+		// Rocks south wall
+		rock = Instantiate(rocks[Random.Range(0, 4)], Vector3(r * 1.0f, 0.0f, -16.0f), Quaternion.Euler(0.0f, Random.value * 360.0f, 0.0f));
+		rock.name = "SouthWall";
+		// Rocks west wall
+		rock = Instantiate(rocks[Random.Range(0, 4)], Vector3(-16.0f, 0.0f, r * 1.0f), Quaternion.Euler(0.0f, Random.value * 360.0f, 0.0f));
+		rock.name = "WestWall";
+	}
 }
 
 function Update () {
